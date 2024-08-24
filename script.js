afba735d713e3f3ce2053e3f045af5b51a0bf9a5
Alain Bonneau <alaindev97@gmail.com>
2024-08-24 03:08:30 +0200

    add tsconfig to es6

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-var GuessedNumber = document.getElementById("guess-number");
-var form = document.getElementById("form");
-var goodNumber = document.getElementById("good-number");
-var timeLeft = document.getElementById("time-left");
-var replayBtn = document.getElementById("replay-btn");
-var tryCount = document.getElementById("try-count");
-var gameOver = document.getElementById("game-over");
-var randomNumber = Math.round(Math.random() * 1000);
-var counter = 1;
-var tryCounter = 10;
-var isGameOver = false;
-var isCountOn = false;
+const GuessedNumber = document.getElementById("guess-number");
+const form = document.getElementById("form");
+const goodNumber = document.getElementById("good-number");
+const timeLeft = document.getElementById("time-left");
+const replayBtn = document.getElementById("replay-btn");
+const tryCount = document.getElementById("try-count");
+const gameOver = document.getElementById("game-over");
+let randomNumber = Math.round(Math.random() * 1000);
+let counter = 1;
+let tryCounter = 10;
+let isGameOver = false;
+let isCountOn = false;
 // Fonction qui permet de rafraîchir la page.
 function refreshPage() {
     location.reload();
@@ -21,12 +21,12 @@ function replayButton() {
 // fonction qui diminue le nombre d'essai du joueur à chaque fois qu'il se trompe
 function gameCounter() {
     tryCounter--;
-    var attemptWord = tryCounter === 1 ? 'essai' : 'essais';
-    tryCount.innerHTML = "Il vous reste <span class=\"span-good\">".concat(tryCounter, "</span> ").concat(attemptWord);
+    const attemptWord = tryCounter === 1 ? 'essai' : 'essais';
+    tryCount.innerHTML = `Il vous reste <span class="span-good">${tryCounter}</span> ${attemptWord}`;
     if (tryCounter === 0) {
         isGameOver = true;
-        tryCount.innerHTML = "Vous avez <span class=\"span-good\">\u00E9puis\u00E9</span> tous vos essais.";
-        timeLeft.innerHTML = "Temps \u00E9coul\u00E9 ! Le bon nombre \u00E9tait : <span class=\"span-good\">".concat(randomNumber, "</span>. Vous n'avez pas trouv\u00E9 le nombre en <span class=\"span-good\">").concat(counter - 1, "</span> essais.");
+        tryCount.innerHTML = `Vous avez <span class="span-good">épuisé</span> tous vos essais.`;
+        timeLeft.innerHTML = `Temps écoulé ! Le bon nombre était : <span class="span-good">${randomNumber}</span>. Vous n'avez pas trouvé le nombre en <span class="span-good">${counter - 1}</span> essais.`;
         blockInput();
         gameOverBtn();
         replayButton();
@@ -41,20 +41,20 @@ function gameOverBtn() {
 }
 // Fonction qui calcule le temps qu'il reste dans la partie et l'affiche sur la page.
 function timeCalcul() {
-    var countDown = new Date().getTime() + 60000;
-    var x = setInterval(function () {
-        var timeNow = new Date().getTime();
-        var distance = countDown - timeNow;
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    const countDown = new Date().getTime() + 60000;
+    const x = setInterval(function () {
+        const timeNow = new Date().getTime();
+        const distance = countDown - timeNow;
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
         if (isGameOver) {
             clearInterval(x);
         }
         if (!isGameOver) {
-            timeLeft.innerHTML = "Il vous reste <span class=\"span-good\">".concat(seconds, "</span> secondes !");
+            timeLeft.innerHTML = `Il vous reste <span class="span-good">${seconds}</span> secondes !`;
         }
         if (seconds <= 0) {
             clearInterval(x);
-            timeLeft.innerHTML = "Temps \u00E9coul\u00E9 ! Le bon nombre \u00E9tait : <span class=\"span-good\">".concat(randomNumber, "</span>. Vous n'avez pas trouv\u00E9 le nombre en <span class=\"span-good\">").concat(counter - 1, "</span> essais.");
+            timeLeft.innerHTML = `Temps écoulé ! Le bon nombre était : <span class="span-good">${randomNumber}</span>. Vous n'avez pas trouvé le nombre en <span class="span-good">${counter - 1}</span> essais.`;
             isGameOver = true;
             blockInput();
             gameOverBtn();
@@ -69,7 +69,7 @@ function blockInput() {
 //Fonction principale de l'application, c'est elle qui se lance une fois que l'utilisateur clique sur le bouton "Jouer". Elle indique à celui-ci si le nombre qu'il donne est trop petit, trop grand ou s'il s'agit du bon numéro.
 function userGuess(e) {
     e.preventDefault();
-    var parseGuessedNumber = parseInt(GuessedNumber.value, 10);
+    const parseGuessedNumber = parseInt(GuessedNumber.value, 10);
     if (isNaN(parseGuessedNumber)) {
         goodNumber.innerHTML = "Veuillez entrer un nombre valide !";
         return;
@@ -81,7 +81,7 @@ function userGuess(e) {
         isCountOn = true;
     }
     if (parseGuessedNumber === randomNumber) {
-        goodNumber.innerHTML = "Vous avez trouv\u00E9 le bon nombre : <span class=\"span-good\">".concat(randomNumber, "</span> en <span class=\"span-good\">").concat(counter - 1, "</span> essais. F\u00E9licitation !");
+        goodNumber.innerHTML = `Vous avez trouvé le bon nombre : <span class="span-good">${randomNumber}</span> en <span class="span-good">${counter - 1}</span> essais. Félicitation !`;
         isGameOver = true;
         blockInput();
         replayButton();
@@ -90,13 +90,13 @@ function userGuess(e) {
         GuessedNumber.value = "";
         counter++;
         gameCounter();
-        goodNumber.innerHTML = "C'est plus que <span class=\"span-good\">".concat(parseGuessedNumber, "</span>");
+        goodNumber.innerHTML = `C'est plus que <span class="span-good">${parseGuessedNumber}</span>`;
     }
     else if (parseGuessedNumber > randomNumber) {
         GuessedNumber.value = "";
         counter++;
         gameCounter();
-        goodNumber.innerHTML = "C'est moins que <span class=\"span-good\">".concat(parseGuessedNumber, "</span>");
+        goodNumber.innerHTML = `C'est moins que <span class="span-good">${parseGuessedNumber}</span>`;
     }
     else {
         goodNumber.innerHTML = "C'est vide...";
